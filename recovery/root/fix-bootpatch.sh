b4ef931587c63067fe0fd90461acce33823217d4
Joel Stein <js@funkturm.net>
2019-04-24 19:53:11 +0200

    update fix-bootpatch.sh

--- recovery/root/fix-bootpatch.sh
+++ recovery/root/fix-bootpatch.sh
@@ -1,16 +1,18 @@
 #!/sbin/sh
 
-PAYLOAD_BLOCK=223223
+dd if=/dev/block/platform/mtk-msdc.0/by-name/recovery of=/tmp/recovery_amonet.hdr bs=512 count=2
+dd if=/dev/block/platform/mtk-msdc.0/by-name/boot of=/tmp/boot_amonet.hdr bs=512 count=2
+dd if=/dev/block/platform/mtk-msdc.0/by-name/boot of=/tmp/boot_amonet.hdr2 bs=512 count=2 skip=2
 
-dd if=/dev/block/platform/mtk-msdc.0/by-name/recovery_amonet of=/tmp/recovery_amonet.hdr bs=512 count=1
-dd if=/dev/block/platform/mtk-msdc.0/by-name/recovery_amonet of=/tmp/recovery_amonet.payload bs=512 skip=$PAYLOAD_BLOCK
-dd if=/dev/block/platform/mtk-msdc.0/by-name/boot_amonet of=/tmp/boot_amonet.hdr bs=512 count=1
-dd if=/dev/block/platform/mtk-msdc.0/by-name/boot_amonet of=/tmp/boot_amonet.payload bs=512 skip=$PAYLOAD_BLOCK
-
-diff /tmp/recovery_amonet.hdr /tmp/boot_amonet.hdr && diff /tmp/recovery_amonet.payload /tmp/boot_amonet.payload
+diff /tmp/recovery_amonet.hdr /tmp/boot_amonet.hdr
 
 if [ $? -ne 0 ] ; then
-    echo "Detected overwritten boot-exploit, restoring from recovery" > /tmp/restore_bootpatch.log
-    dd if=/tmp/recovery_amonet.hdr of=/dev/block/platform/mtk-msdc.0/by-name/boot_amonet bs=512 count=1
-    dd if=/tmp/recovery_amonet.payload of=/dev/block/platform/mtk-msdc.0/by-name/boot_amonet bs=512 seek=$PAYLOAD_BLOCK
+    echo "Detected old or overwritten boot-exploit, restoring from recovery" > /tmp/restore_bootpatch.log
+    grep "ANDROID!" /tmp/boot_amonet.hdr2
+    if [ $? -ne 0 ] ; then
+    	echo "Copy header to block 2" >> /tmp/restore_bootpatch.log
+    	dd if=/tmp/boot_amonet.hdr of=/dev/block/platform/mtk-msdc.0/by-name/boot bs=512 count=2 seek=2	
+    fi
+
+    dd if=/tmp/recovery_amonet.hdr of=/dev/block/platform/mtk-msdc.0/by-name/boot bs=512 count=2
 fi
